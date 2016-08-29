58659b4c3c76db140533f2ec94014a6d911a5ea0
E. R. C. Pereda <ercp42@gmail.com>
2016-08-29 19:57:48 -0300

    feat(index): Add a function for retrive only the urls of the images

--- src/index.js
+++ src/index.js
@@ -16,15 +16,29 @@ export const MKT = {
 };
 
 /**
- * Gets the urls of the bing's images of the day.
+ * Gets the raw object returned by bing's images of the day service.
  * @param {number} [n=1] - How many images to return.
  * @param {MKT} [mkt=en-US] - Tells which of the 8 markets Bing is available for you would like images from.
  * @param {number} [idx=0] - Tells where you want start from. 0 would start at the current day, 1 the previous day, etc.
  * @param {string} [format=js] - Set the format in which the data will be return.
  * @returns {Promise.<Object, Error>} A promise that return the image data if resolved, or an Error if rejected
  */
-export async function getImages(n = 1, mkt = MKT.enUS, idx = 0, format = 'js') {
+export async function getRaw(n = 1, mkt = MKT.enUS, idx = 0, format = 'js') {
   let res = await request.get(`http://www.bing.com/HPImageArchive.aspx?format=${format}&idx=${idx}&n=${n}&mkt=${mkt}`);
 
   return JSON.parse(res.body);
+}
+
+/**
+ * Gets the urls of the bing's images of the day service.
+ * @param {number} [n=1] - How many images to return.
+ * @param {MKT} [mkt=en-US] - Tells which of the 8 markets Bing is available for you would like images from.
+ * @param {number} [idx=0] - Tells where you want start from. 0 would start at the current day, 1 the previous day, etc.
+ * @param {string} [format=js] - Set the format in which the data will be return.
+ * @returns {Promise.<Object, Error>} A promise that return the image data if resolved, or an Error if rejected
+ */
+export async function getUrls(n = 1, mkt = MKT.enUs, idx = 0, format = 'js') {
+  let res = await getRaw(n, mkt, idx, format);
+
+  return res.images.map(i => `https://www.bing.com${i.url}`);
 }
--- src/index.spec.js
+++ src/index.spec.js
@@ -2,7 +2,15 @@ import {expect} from 'chai';
 import lib from './index';
 
 describe('bing-image-of-the-day', () => {
-  it('should be work', () => {
-    expect(true).to.true;
+  it('async test should be work', async function () {
+    var testPromise = new Promise(function(resolve, reject) {
+      setTimeout(function() {
+        resolve('Hello World!');
+      }, 200);
+    });
+
+    var result = await testPromise;
+
+    expect(result).to.equal('Hello World!');
   });
 });
